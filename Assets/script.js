8c34e12feb16899ad92cece3b095cb8d1f7296c4
JCool221 <109809337+JCool221@users.noreply.github.com>
2022-10-15 15:33:34 -0400

    minor changes

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,36 @@
 var timeDisplayEl = $('#currentDay');
+var nineAM = $('#morning');
+var hourHolderEl = $('#testdiv');
+var thingy = $('#testdiv').text();
 
 // handle displaying the time
 function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
-  }
-  
+  var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.text(rightNow);
+  // check the time badge and do function
+  var hour = moment().format('k');
+  hourHolderEl.text(hour);
+  // console.log(hour);
+}
 
+// change the Highlight of the schedule bar to the current hour
+function Now () {
+  nineAM.removeClass('future ').addClass('present');
+}
 
+// change the Highlight of the schedule bar to a past hour
+function past () {
+  nineAM.removeClass('present').addClass('past');
+}
 
-setInterval(displayTime, 1000);
+// change the Highlight of the schedule bar to a future hour
+function future () {
+  nineAM.removeClass('past').addClass('future');
+}
+
+function currentHour () {
+  console.log(hourHolderEl);
+}
+var hourNow = $('#hourHolder').text();
+  // timer
+    setInterval(displayTime, 1000);
