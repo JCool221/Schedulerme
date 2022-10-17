da87ee707e58b9940e25cfb507187e7f944c8174
JCool221 <109809337+JCool221@users.noreply.github.com>
2022-10-17 17:50:02 -0400

    added all time blocks

--- Assets/script.js
+++ Assets/script.js
@@ -4,6 +4,10 @@ var tenAM = $('#10am');
 var elevenAM = $('#11am');
 var twelvePM = $('#12pm');
 var onePM = $('#1pm');
+var twoPM = $('#2pm');
+var threePM = $('#3pm');
+var fourPM = $('#4pm');
+var fivePM = $('#5pm');
 
 
 // handle displaying the time
@@ -17,35 +21,39 @@ function renderBars() {
   // var badgeHour = ($("div#badge").text().trim());
   // console.log(currentHour);
 
-    if (9>currentHour) {
+    if (9<currentHour) {
           nineAM.removeClass('present').removeClass('future').addClass('past');
       } else if (9==currentHour) {
           nineAM.removeClass('future').removeClass('past').addClass('present');
       } else if (9>currentHour) {
           nineAM.removeClass('past').removeClass('present').addClass('future');
       }
-    if (10>currentHour) {
+
+    if (10<currentHour) {
           tenAM.removeClass('present').removeClass('future').addClass('past');
       } else if (10==currentHour) {
           tenAM.removeClass('future').removeClass('past').addClass('present');
       } else if (10>currentHour) {
           tenAM.removeClass('past').removeClass('present').addClass('future');
       }
-    if (11>currentHour) {
+      
+    if (11<currentHour) {
           elevenAM.removeClass('present').removeClass('future').addClass('past');
       } else if (11==currentHour) {
           elevenAM.removeClass('future').removeClass('past').addClass('present');
       } else if (11>currentHour) {
           elevenAM.removeClass('past').removeClass('present').addClass('future');
       }
-    if (12>currentHour) {
+
+    if (12<currentHour) {
           twelvePM.removeClass('present').removeClass('future').addClass('past');
       } else if (12==currentHour) {
           twelvePM.removeClass('future').removeClass('past').addClass('present');
       } else if (12>currentHour) {
           twelvePM.removeClass('past').removeClass('present').addClass('future');
       }
-    if (13>currentHour) {
+
+    if (13<currentHour) {
             onePM.removeClass('present').removeClass('future').addClass('past');
         } else if (13==currentHour) {
             onePM.removeClass('future').removeClass('past').addClass('present');
@@ -53,6 +61,39 @@ function renderBars() {
             onePM.removeClass('past').removeClass('present').addClass('future');
         }
 
+    if (14<currentHour) {
+            twoPM.removeClass('present').removeClass('future').addClass('past');
+        } else if (14==currentHour) {
+            twoPM.removeClass('future').removeClass('past').addClass('present');
+        } else if (14>currentHour) {
+            twoPM.removeClass('past').removeClass('present').addClass('future');
+        }
+
+    if (15<currentHour) {
+            threePM.removeClass('present').removeClass('future').addClass('past');
+        } else if (15==currentHour) {
+            threePM.removeClass('future').removeClass('past').addClass('present');
+        } else if (15>currentHour) {
+            threePM.removeClass('past').removeClass('present').addClass('future');
+        }
+
+    if (16<currentHour) {
+            fourPM.removeClass('present').removeClass('future').addClass('past');
+        } else if (16==currentHour) {
+            fourPM.removeClass('future').removeClass('past').addClass('present');
+        } else if (16>currentHour) {
+            fourPM.removeClass('past').removeClass('present').addClass('future');
+        }
+
+    if (17<currentHour) {
+            fivePM.removeClass('present').removeClass('future').addClass('past');
+        } else if (17==currentHour) {
+            fivePM.removeClass('future').removeClass('past').addClass('present');
+        } else if (17>currentHour) {
+            fivePM.removeClass('past').removeClass('present').addClass('future');
+        }
+
+console.log(currentHour);
 }   
 
 // timer
