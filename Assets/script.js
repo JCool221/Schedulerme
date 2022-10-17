fab87a1cd279c6c73fa5cf57b94b3eca01ca441d
JCool221 <109809337+JCool221@users.noreply.github.com>
2022-10-16 20:31:18 -0400

    added additional timeblocks

--- Assets/script.js
+++ Assets/script.js
@@ -1,37 +1,51 @@
 var timeDisplayEl = $('#currentDay');
-var nineAM = $('#morning');
+var nineAM = $('#9am');
+var tenAM = $('#10am');
+var elevenAM = $('#11am');
+var twelvePM = $('#12pm');
+
 // handle displaying the time
 function displayTime() {
   var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
   timeDisplayEl.text(rightNow);
-  
+} 
   // check the time badge and set the bar colors
+function renderBars() {
   var currentHour = moment().format('k') 
-  var badgeHour = ($("div#badge").text().trim());
-  console.log(currentHour);
-    if (badgeHour>currentHour) {
-      nineAM.removeClass('present').removeClass('future').addClass('past');
-    } else if (badgeHour==currentHour) {
-      nineAM.removeClass('future').removeClass('past').addClass('present');
-    } else if (badgeHour>currentHour) {
-      nineAM.removeClass('past').removeClass('present').addClass('future');
-    }
-    
-}
-//   // change the Highlight of the schedule bar to the current hour
-//   function present () {
-//     present();
-//   }
-  
-//   // change the Highlight of the schedule bar to a past hour
-//   function past () {
-//     past();
-//   }
-  
-//   // change the Highlight of the schedule bar to a future hour
-//   function future () {
-//   future();
-// }
+  // var badgeHour = ($("div#badge").text().trim());
+  // console.log(currentHour);
+
+    if (22>currentHour) {
+          nineAM.removeClass('present').removeClass('future').addClass('past');
+      } else if (22==currentHour) {
+          nineAM.removeClass('future').removeClass('past').addClass('present');
+      } else if (22>currentHour) {
+          nineAM.removeClass('past').removeClass('present').addClass('future');
+      }
+    if (20>currentHour) {
+          tenAM.removeClass('present').removeClass('future').addClass('past');
+      } else if (20==currentHour) {
+          tenAM.removeClass('future').removeClass('past').addClass('present');
+      } else if (20>currentHour) {
+          tenAM.removeClass('past').removeClass('present').addClass('future');
+      }
+    if (21>currentHour) {
+          elevenAM.removeClass('present').removeClass('future').addClass('past');
+      } else if (21==currentHour) {
+          elevenAM.removeClass('future').removeClass('past').addClass('present');
+      } else if (21>currentHour) {
+          elevenAM.removeClass('past').removeClass('present').addClass('future');
+      }
+    if (21>currentHour) {
+          twelvePM.removeClass('present').removeClass('future').addClass('past');
+      } else if (21==currentHour) {
+          twelvePM.removeClass('future').removeClass('past').addClass('present');
+      } else if (21>currentHour) {
+          twelvePM.removeClass('past').removeClass('present').addClass('future');
+      }
+
+}   
 
 // timer
    setInterval(displayTime, 1000);
+   setInterval(renderBars, 1000);
